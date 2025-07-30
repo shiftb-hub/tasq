ca6635506f84f77f0e839e87434c54b7f6f960b8
T.Wada <28886385+TakeshiWada1980@users.noreply.github.com>
2025-07-30 09:22:25 +0900

    プロフィール設定系の型を追加

--- src/app/_types/CommonSchemas.ts
+++ src/app/_types/CommonSchemas.ts
@@ -1,8 +1,52 @@
+import { Role } from "@prisma/client";
 import { z } from "zod";
 
-export const passwordSchema = z.string().min(5);
 export const emailSchema = z.email();
-export const userNameSchema = z.string().min(1);
+export const passwordSchema = z.string().min(5);
+
+export const userNameSchema = z
+  .string()
+  .min(1)
+  .max(16)
+  .refine((val) => !/^[\s\u3000]|[\s\u3000]$/.test(val), {
+    message: "前後に空白文字を含めることはできません。",
+  });
+
+export const roleSchema = z
+  .string()
+  .transform((val) => val.toUpperCase())
+  .refine((val): val is Role => Object.values(Role).includes(val as Role))
+  .transform((val) => val as Role)
+  .optional();
+
+export const chapterSchema = z.int().min(1).max(14).optional();
+
+export const jobSchema = z
+  .string()
+  .max(16)
+  .transform((val) => (val.trim() === "" ? undefined : val))
+  .optional();
+
+export const slackIdSchema = z
+  .string()
+  .transform((val) => (val.trim() === "" ? undefined : val)) // 空文字なら undefined に変換
+  .refine((val) => val === undefined || val.startsWith("@"), {
+    message: "Slack ID は @ を含めて入力してください。",
+  })
+  .optional();
+
+export const snsIdSchema = z
+  .string()
+  .max(64)
+  .transform((val) => (val.trim() === "" ? undefined : val))
+  .optional();
+
+export const bioSchema = z.string().max(1000);
+
+export const profileImageKeySchema = z
+  .string()
+  .transform((val) => (val.trim() === "" ? undefined : val))
+  .optional();
 
 // prettier-ignore
 export const isUUID = (value: string) => /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(value);
