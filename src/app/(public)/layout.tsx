9df20cea773c25b8b84a20f0c27a7b58302ddc3d
k7d0112 <k7dworks@gmail.com>
2025-08-03 23:21:28 +0900

    /ページにアクセスした時に/loginページにリダイレクトされる問題の修正

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from "next";
+import "../globals.css";
+
+export const metadata: Metadata = {
+  title: "TASQ",
+  description: "...",
+};
+
 type Props = {
   children: React.ReactNode;
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-import { redirect } from "next/navigation";
+import Link from "next/link";
+import { DevAuthButton } from "@/app/_components/DevAuthButton";
 
 const Page: React.FC = () => {
-  redirect("/login");
+  return (
+    <div className="mx-4 mt-2 max-w-3xl md:mx-auto">
+      <div className="text-2xl font-bold">Main</div>
+      <div className="mt-4 ml-2 flex flex-col gap-y-2">
+        <Link href="/signup">サインアップ</Link>
+        <Link href="/login">ログイン</Link>
+        <Link href="/api/playground/tasks">APIテスト（public）</Link>
+        <Link href="/api/playground/users">APIテスト（private）</Link>
+        <Link href="/settings">設定（private）</Link>
+        <div className="mt-4">
+          <DevAuthButton />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
